493d5a36818f55bd1bd38b9812ac770894a3263a
Heng WU <wuheng@iscas.ac.cn>
2021-08-03 10:57:58 +0800

    add more tests

--- src/main/java/io/github/kubesys/tongtech/Main.java
+++ src/main/java/io/github/kubesys/tongtech/Main.java
@@ -13,13 +13,26 @@ package io.github.kubesys.tongtech;
 public class Main {
 
 	public static void main(String[] args) {
+		loadJsr284("javax.rcm.management.ResourceAttributesMBean");
+		loadJsr284("javax.rcm.management.ResourceDomainMBean");
+		loadJsr284("javax.rcm.Constraint");
+		loadJsr284("javax.rcm.Notification");
+		loadJsr284("javax.rcm.ResourceAttributes");
+		loadJsr284("javax.rcm.ResourceAttributesFactory");
+		loadJsr284("javax.rcm.ResourceConsumer");
+		loadJsr284("javax.rcm.ResourceDomain");
+		loadJsr284("javax.rcm.ResourceException");
+		loadJsr284("javax.rcm.ResourcePermission");
+		System.out.print("Current JDK supports JSR284");
+	}
+
+	public static void loadJsr284(String classname) {
 		try {
-			Class.forName("javax.rcm.management.ResourceAttributesMBean");
+			Class.forName(classname);
 		} catch (ClassNotFoundException e) {
 			System.out.println("Current JDK does not support JSR284");
 			e.printStackTrace();
 			return;
 		}
-		System.out.print("Current JDK supports JSR284");
 	}
 }
